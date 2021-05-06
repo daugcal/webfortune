77e72812b2eb70e21de8411aca9e8013bd8668b5
daugcal <calebdaugh@hotmail.com>
2021-05-06 01:55:06 -0500

    working version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# webfortune
-Final Project for DevOps
+# Final Project for DevOps: webfortune
+In order to use this web application, you must first clone this repository
+Then, run the following command within the repositories directory.
+'docker build -t webfortune .'
+
+To then run the web application, type the following.
+'docker run -dp <port>:5000 webfortune' where ,port. is the port you wish to access the application on.
--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from Flask import Flask
+from flask import *
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, from Caleb!"
-
+    return redirect(url_for('fortune'))
+@app.route('/fortune/')
+def fortune():
+    # strings "<pre>" and "</pre>".
+    result = os.popen('fortune').read()
+    return ("<pre>"+result+"</pre>")    
 @app.route('/cowsay/<message>/')
 def cowsay(message):
     # Code goes here.
-    return "not yet"
+    result = os.popen('cowsay '+message).read()
+    return ("<pre>"+result+"</pre>")    
+    return message
+@app.route('/cowfortune/')
+def cowfortune():
+    # Code goes here.
+    fortune = os.popen('fortune').read()
+    result = os.popen('cowsay '+fortune).read()
+    return ("<pre>"+result+"</pre>")    
+    return message
